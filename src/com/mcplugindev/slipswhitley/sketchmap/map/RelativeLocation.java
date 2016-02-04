30e79e75f013c893623ee69fbf6975d35137c80e
trainphreak <trainphreak@users.noreply.github.com>
2016-02-03 17:50:17 -0800

    Added support for survival mode via a new subcommand: getmap Players must have an empty map item in their inventory to use the command and can only get one map item per command issued. If the player has more than one empty map item in their inventory, they must also have an empty slot for the sketchmap item; if there is no empty slot for the sketchmap, the empty map item will be returned. In keeping with the coordinate system in the map items' lore, the coordinates for this command start at 1:1, even though Sketchmap internally uses 0:0-based coords.
    Adjusted the syntax help for /sketchmap create and /sketchmap import
    Added functionality to the RelativeLocation class to be used in /sktechmap getmap

--- src/com/mcplugindev/slipswhitley/sketchmap/map/RelativeLocation.java
+++ src/com/mcplugindev/slipswhitley/sketchmap/map/RelativeLocation.java
@@ -19,6 +19,11 @@ public class RelativeLocation
 
     public static RelativeLocation fromString(final String str)
     {
+        return fromStringZeroBase(str);
+    }
+
+    public static RelativeLocation fromStringZeroBase(final String str)
+    {
         final String[] args = str.split(":");
         if (args.length != 2)
         {
@@ -30,12 +35,49 @@ public class RelativeLocation
         {
             x = Integer.parseInt(args[0]);
             y = Integer.parseInt(args[1]);
+            if (x < 0 || y < 0)
+                throw new Exception("negative coordinates are disallowed");
         }
         catch (Exception ex)
         {
             return null;
         }
         return new RelativeLocation(x, y);
+    }
+
+    public static RelativeLocation fromStringOneBase(final String str)
+    {
+        final String[] args = str.split(":");
+        if (args.length != 2)
+        {
+            return null;
+        }
+        int x;
+        int y;
+        try
+        {
+            x = Integer.parseInt(args[0]) - 1;
+            y = Integer.parseInt(args[1]) - 1;
+            if (x < 0 || y < 0)
+                throw new Exception("negative coordinates are disallowed");
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
+        return new RelativeLocation(x, y);
+    }
+
+    /**
+     * Compares two RelativeLocation objects to see if they point to the same relative coordinates. Does NOT check
+     * whether the coordinates are on the same sketchmap.
+     *
+     * @param otherLoc RelativeLocation object to be compared
+     * @return true if the relative X and Y coordinates are the same for both objects, false otherwise
+     */
+    public boolean equals(RelativeLocation otherLoc)
+    {
+        return this.x == otherLoc.x && this.y == otherLoc.y;
     }
 
     public int getX()
